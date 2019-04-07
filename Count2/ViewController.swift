1b57d91bb6256b438449c5f8a566f43f0f1fe53d
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-07 17:29:35 +0900

    minus,clearボタンの追加，全ボタンを角丸に

--- Count2/ViewController.swift
+++ Count2/ViewController.swift
@@ -15,22 +15,29 @@ class ViewController: UIViewController {
     var number: Int = 0
     @IBOutlet var label: UILabel!  //@IBOutlet部品を宣言
     @IBOutlet var button: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         button.layer.cornerRadius = 10.0
-        label.layer.cornerRadius = 10.0
-        
-        
+        button2.layer.cornerRadius = 10.0
+        button3.layer.cornerRadius = 10.0
     }
     @IBAction func plus() { //好きに関数名決めていい //() {隙間開ける
         number = number + 1
         label.text = String(number) //さっき宣言したlabelのテキストに文字列を表示
         //label.backgroundColor = UIColor.red
         label.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
-        
-        
+    }
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number) //さっき宣言したlabelのテキストに文字列を表示
+    }
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
     }
     
 
